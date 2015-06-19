305d109fbd7ca8d6ea4725fc10752b9591b31551
Nithya Rao <bumblebeenr@gmail.com>
2015-06-18 22:06:11 -0500

    Completed Checkpoint-32-seeddata-assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,30 @@
 require 'faker'
  
- # Create Posts
- 50.times do
-   Post.create!(
-     title:  Faker::Lorem.sentence,
-     body:   Faker::Lorem.paragraph
-   )
- end
- posts = Post.all
+ # Create Posts only if it does not exist 
+ title = "Black Hawks"
+ body = "The Chicago Blackhawks are a professional ice hockey team based in Chicago, Illinois.They have won 3 Stanley cups in 6 years"  
  
- # Create Comments
- 100.times do
-   Comment.create!(
-     post: posts.sample,
-     body: Faker::Lorem.paragraph
+ post = Post.find_by(title: title) 
+ 
+ if post.nil?
+  # doesn't exists in Database
+   Post.create!(
+     title:  "Black Hawks",
+     body:   "Stanley Cup Winner"
    )
+else
+  # Exists in Database 
+   puts "#{title} exists in Database" 
+end
+
+if Comment.find_by(post: post, body: body ).nil?
+  # comment does not exists
+   Comment.create!(
+     post: post,
+    body: body
+   )
+ else
+   puts "#{title} and #{body} exists in Database"   
  end
  
  puts "Seed finished"
