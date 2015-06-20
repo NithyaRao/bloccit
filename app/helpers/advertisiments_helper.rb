f22f4ee5444f75defcf7314972ec0ded104e6c47
Nithya Rao <bumblebeenr@gmail.com>
2015-06-20 16:50:33 -0500

    changed

---
+++ app/helpers/advertisiments_helper.rb
@@ -0,0 +1,2 @@
+module AdvertisimentsHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'advertisiments/index'
+
+  get 'advertisiments/show'
+
   resources :posts
+  resources :advertisements
+
   get 'about' =>'welcome#about'
   
   root to: 'welcome#index'
