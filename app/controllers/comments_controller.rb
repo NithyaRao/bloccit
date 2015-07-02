3e09afed6d9825fecd21143c9894a7282bea4456
Nithya Rao <bumblebeenr@gmail.com>
2015-07-01 21:34:50 -0500

    completed checkpoint-47-Destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,9 +18,22 @@ class CommentsController < ApplicationController
      else
        flash[:error] = "There was an error saving the comment. Please try again."
        render "posts/show" 
-     end
-       
+     end      
    end
+
+  def destroy
+     @topic = Topic.find(params[:topic_id])
+     @post = Post.find(params[:post_id])
+     @comment = Comment.find(params[:id])
+     authorize @comment
+     if @comment.destroy
+      flash[:notice] = "Comment was removed."
+       redirect_to [@topic, @post]
+     else
+       flash[:error] = "There was an error removing the comment. Please try again."
+       render 'posts/show'
+     end
+  end
 
   def comment_params
      params.require(:comment).permit(:body)
--- app/policies/topic_policy.rb
+++ app/policies/topic_policy.rb
@@ -11,4 +11,7 @@ class TopicPolicy < ApplicationPolicy
    def update?
      create?
    end
+   def destroy?
+     create?
+   end
  end
