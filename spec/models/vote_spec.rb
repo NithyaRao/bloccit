10fe28a4c0e86f29d1e7623fca44723b8cd967e6
Nithya Rao <bumblebeenr@gmail.com>
2015-07-07 15:42:50 -0500

    In progress checkpoint-49-Voting

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,5 +1,22 @@
-require 'rails_helper'
-
-RSpec.describe Vote, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+describe Vote do
+  describe "validations" do
+       let(:vote) {Vote.new}
+        it "only allows 1 as value" do
+           @vote.value = 1
+           expect (@vote.value).to eq(1)
+           expect( @vote.valid?).to be_true 
+        end
+        it "only allows -1 as value" do
+           @vote.value = -1
+           expect (@vote.value).to eq(-1)
+           expect ( @vote.valid?).to be_true
+        end
+        it "it prohibits other values" do
+           @vote.value = 0 
+           expect( @vote.valid?).to  be_false
+           @vote.value = 2
+           expect( @vote.valid?).to  be_false
+        end
+  end
+end  
+    
