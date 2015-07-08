33c41e51c3b5a59d9e91fd201cb98d3286b4bc4a
Nithya Rao <bumblebeenr@gmail.com>
2015-07-07 20:53:58 -0500

    Inprogress checkpoint-49-Voting before merging checkpoint-47-Destroy-assignment

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,12 @@ Rails.application.routes.draw do
   resources :users, only: [:update]
   resources :topics do 
     resources :posts, except: [:index] do
-        resources :comments, only: [:create, :destroy]
+         resources :comments, only: [:create, :destroy]
+         post '/up-vote' => 'votes#up_vote', as: :up_vote
+         post '/down-vote' => 'votes#down_vote', as: :down_vote
         end
    end    
+
   get 'about' =>'welcome#about'
   
   root to: 'welcome#index'
