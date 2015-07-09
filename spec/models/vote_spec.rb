48813d1279170ac47228fde0eedd15d6b1d8616f
Nithya Rao <bumblebeenr@gmail.com>
2015-07-09 15:12:51 -0500

    Completed checkpoint-50-MockingRSpec

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe Vote do
+  include TestFactories
   describe "validations" do
        let(:vote) {Vote.new}
         it "only allows 1 as value" do
@@ -18,5 +19,13 @@ describe Vote do
            expect( vote.valid?).to  eq(false)
         end
   end
+  describe 'after_save' do
+     it "calls `Post#update_rank` after save" do
+       post = associated_post
+       vote = Vote.new(value: 1, post: post)
+       expect(post).to receive(:update_rank)
+       vote.save
+     end
+   end
 end  
-    
+
