9402a7abeaa03e8b2d8c1914dac99679ab895837
Nithya Rao <bumblebeenr@gmail.com>
2015-07-09 20:20:01 -0500

    Completed checkpoint-50-MockingRSpec-assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -32,4 +32,12 @@ class Post < ActiveRecord::Base
   def create_vote
       user.votes.create(value: 1 , post: self )
   end
+
+  def save_with_initial_vote
+     ActiveRecord::Base.transaction do
+       if self.save
+           create_vote
+       end 
+    end   
+  end
 end
