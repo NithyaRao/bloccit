b82ccc1f3807ab94690859688a2841a7c77435cd
Nithya Rao <bumblebeenr@gmail.com>
2015-07-10 20:08:28 -0500

    Completed checkpoint-51-favoriting-assignment

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,19 +3,26 @@ require 'rails_helper'
  describe User do
  
    include TestFactories
- 
-   describe "#favorited(post)" do
-    before do
+
+   before do
       @post = associated_post
       @user = authenticated_user
+      @post_1 = associated_post  
     end
+ 
+   describe "#favorited(post)" do
+    
      it "returns `nil` if the user has not favorited the post" do
-        expect (@user.favorites.find_by_post_id(@post_id)).to be_nil    
+       expect( @user.favorites.find_by_post_id(@post.id) ).to be_nil    
      end
  
      it "returns the appropriate favorite if it exists" do
-        @favorite = @user.favorites.where(post: @post).create
-         expect (@user.favorites.find_by_post_id(@post.id)).to eq(@favorite)
+        @favorite = @user.favorites.create(post: @post)
+         expect( @user.favorites.find_by_post_id(@post.id) ).to eq(@favorite)
+     end
+     it "returns `nil` if the user has favorited another post" do
+        @favorite = @user.favorites.create(post: @post_1)
+        expect( @user.favorites.find_by_post_id(@post.id) ).to be_nil  
      end
    end
  end
