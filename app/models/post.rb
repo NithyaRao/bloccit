427833a00990bcc0f04bad8fda1b36572d4c74bd
Nithya Rao <bumblebeenr@gmail.com>
2015-07-13 16:57:40 -0500

    completed checkpoint-54-PublicProfile

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,11 +5,13 @@ class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :topic
   default_scope { order('rank DESC') }
+  scope :visible_to, -> (user) { user ? all : joins(:topic).where('topics.public' => true) }
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
   validates :user, presence: true
+
 
   def up_votes
      votes.where(value: 1).count
